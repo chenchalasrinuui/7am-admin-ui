ec95f8cd94d3d7ceccdc0dde8c206699b8e90d64
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2024-08-30 09:09:59 +0530

    latest

---
+++ src/Login/Login.tsx
@@ -0,0 +1,18 @@
+import { appCtx } from '@/context/appCtx'
+import React, { useContext } from 'react'
+
+export const Login = () => {
+    const ctxData = useContext(appCtx)
+    const fnLogin = () => {
+        ctxData.dispatch({
+            type: "LOGIN",
+            payload: true
+        })
+    }
+    return (
+        <div>Login
+
+            <button onClick={fnLogin}>Login</button>
+        </div>
+    )
+}

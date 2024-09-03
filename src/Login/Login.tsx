dcfd656a9c3c1a6ee432cf2b8b47069700147d26
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2024-09-03 09:07:43 +0530

    latest

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,13 +1,31 @@
 import { Input } from '@/common/components/Input'
 import { appCtx } from '@/context/appCtx'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import config from './config.json'
 export const Login = () => {
+    const [data, setData] = useState({})
     const ctxData = useContext(appCtx)
     const fnLogin = () => {
-        ctxData.dispatch({
-            type: "LOGIN",
-            payload: true
+
+        alert("sending request...")
+        // ctxData.dispatch({
+        //     type: "LOGIN",
+        //     payload: true
+        // })
+    }
+    const handleChange = (eve: any) => {
+        const { name, value } = eve?.target
+        let inputObj: any = config.find((obj) => {
+            return obj.name === name
+        })
+        inputObj.hasError = false;
+        inputObj.value = value
+        if (!value) {
+            inputObj.hasError = true
+        }
+        setData({
+            ...data,
+            [name]: value
         })
     }
     return (
@@ -15,7 +33,7 @@ export const Login = () => {
             <h3 className='mt-3 mb-3 text-center'>Login</h3>
             {
                 config.map((obj) => {
-                    return <Input {...obj} hanldeChange={() => { }} />
+                    return <Input {...obj} hanldeChange={handleChange} />
                 })
             }
             <div className='row mb-3'>
--- src/common/components/Input/Input.tsx
+++ src/common/components/Input/Input.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export const Input = ({ hanldeChange, lbl, name, type, errorMsg, lblCols, inputCols, errMsgCols }: any) => {
+export const Input = ({ hanldeChange, lbl, isRequired, value, name, type, hasError, errorMsg, lblCols, inputCols, errMsgCols }: any) => {
     return <div className='row mb-3'>
         <div className={`col-sm-${lblCols} text-end`}>
-            <b>{lbl}:</b>
+            <b>{lbl}{isRequired && <span className='text-danger'>*</span>}:</b>
         </div>
         <div className={`col-sm-${inputCols}`}>
             <input onChange={hanldeChange} name={name} type={type} className='form-control' />
         </div>
         <div className={`col-sm-${errMsgCols}`}>
-            <b className='text-danger'>{errorMsg}</b>
+            {hasError && <b className='text-danger'>{errorMsg}</b>}
         </div>
     </div>
 
